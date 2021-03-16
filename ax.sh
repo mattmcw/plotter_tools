50f05cd151dcaf9716ac200a0d12ceeb76f6a18b
mattmcw <hi@mmcwilliams.com>
2021-03-16 19:39:20 -0400

    Add the axicli wrapper script I use to control my plotter for most drawings

---
+++ ax.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#wrapper script for controlling my RobotHouse A3 plotter with
+#the AxiDraw CLI : https://axidraw.com/doc/cli_api/
+#use the config from this repo and place this script in your path
+#chmod +x and all that
+#I just call it "ax"
+
+axicli -f ./axidraw_conf_robothouse_a3.py \
+	--report_time \
+	--model 2 \
+	--pen_pos_up 20 \
+	--pen_pos_down 60 \
+	"${@}"
